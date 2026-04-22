0bfdd45587906514dc6dec49f998bc6ba1ebfda7
KasenS1230 <kasen.sinclair@cox.net>
2026-04-22 16:43:35 -0500

    fixed event pages

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="relative">
       <Map />
-      <div className="absolute top-4 left-4 z-10">
+      <div className="absolute top-4 right-12 z-10">
         <Link href="/events">
           <button className="px-10 py-3 rounded-full bg-[#[#FDD023] text-[#461D7C] font-bold text-lg shadow-lg bg-yellow-300 transition-all duration-200">
            Events
