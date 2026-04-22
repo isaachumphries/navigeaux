d180d42d806ea87450f52246e49b737c98756424
KasenS1230 <kasen.sinclair@cox.net>
2026-04-22 16:39:44 -0500

    fixed events

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import Start from './start/page';
+import Link from 'next/link';
 
 const Map = dynamic(() => import('./components/map'), { ssr: false });
 
 export default function Home() {
   return (
-    <main>
+    <main className="relative">
       <Map />
+      <div className="absolute top-4 right-12 z-10">
+        <Link href="/events">
+          <button className="px-10 py-3 rounded-full bg-[#[#FDD023] text-[#461D7C] font-bold text-lg shadow-lg bg-yellow-300 transition-all duration-200">
+           Events
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
