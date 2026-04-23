d2b99fe2724c0916f60b1d643c522603b5b4a74d
KasenS1230 <kasen.sinclair@cox.net>
2026-04-23 11:01:27 -0500

    fixed starting page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import Start from './start/page';
 
 const Map = dynamic(() => import('./components/map'), { ssr: false });
 
 export default function Home() {
   return (
     <main className="relative">
-      <Map />
+      <Start />
     </main>
   );
 }
